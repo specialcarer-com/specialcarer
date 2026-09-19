fe785b243db6d28c8e04299b1b3411d72c3d37a6
BYHomeOwner <92626446+BYHomeOwner@users.noreply.github.com>
2026-09-19 09:53:39 +0000

    chore: configure ESLint flat compatibility

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [...compat.config({ extends: ["next"] })];
+
+export default eslintConfig;
